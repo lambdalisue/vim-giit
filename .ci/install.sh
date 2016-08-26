e2c45332dbf40c7fef7c188ebed42e544083101f
lambdalisue <lambdalisue@hashnote.net>
2016-08-27 00:22:22 +0900

    Fix CI script

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,7 +5,5 @@ git config --global user.name "ci"
 git config --global user.email ci@example.com
 git clone -q --depth 1 --single-branch https://github.com/thinca/vim-themis /tmp/vim-themis
 git clone -q --depth 1 --single-branch https://github.com/vim-jp/vital.vim  /tmp/vital
-pip install --user vim-vint
+PYTHONUSERBASE=$HOME/.local pip install --user vim-vint
 bash $root/installer/${TRAVIS_OS_NAME}.sh
-
-
--- .ci/script.sh
+++ .ci/script.sh
@@ -2,19 +2,24 @@
 set -ex
 export PATH="$HOME/.local/bin:$HOME/neovim/bin:$HOME/vim/bin:/tmp/vim-themis/bin:$PATH"
 export THEMIS_HOME="/tmp/vim-themis"
+
 if [[ "$VERSION" == "nvim" ]]; then
-    alias vim='nvim'
+    export THEMIS_VIM="nvim"
     export THEMIS_ARGS="-e -s --headless"
+else
+    export THEMIS_VIM="vim"
 fi
 
 uname -a
-which -a vim
+which -a $THEMIS_VIM
+which -a python
 which -a vint
 which -a themis
 
-vim --version
-vim --cmd "try | helptags doc/ | catch | cquit | endtry" --cmd quit
+$THEMIS_VIM --version
+$THEMIS_VIM --cmd "try | helptags doc/ | catch | cquit | endtry" --cmd quit
 
+python --version
 vint --version
 vint autoload
 
